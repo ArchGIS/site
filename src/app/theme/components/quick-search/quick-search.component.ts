21f51f89769c93c2ef07d73362ec746bc136be6e
nardm <lenar.thg@gmail.com>
2017-08-10 15:15:10 +0300

    module

--- src/app/theme/components/quick-search/quick-search.component.ts
+++ src/app/theme/components/quick-search/quick-search.component.ts
@@ -87,6 +87,19 @@ export class QuickSearchIComponent {
     center: [ 46.879966, -121.726909 ]
   };
 
+  // Fields for managing the form inputs and binding to leaflet zoom/center
+  model = new LeafletCoreDemoModel(
+      this.optionsSpec.center[0],
+      this.optionsSpec.center[1],
+      this.optionsSpec.zoom
+  );
+  zoom: number;
+  center: L.LatLng;
+
+  /*
+   * This are the leaflet map options that we're going to use for input binding
+   */
+
   options = {
     layers: this.optionsSpec.layers.map((l) => {
       return L.tileLayer(l.url, { maxZoom: l.maxZoom, attribution: l.attribution });
@@ -94,6 +107,35 @@ export class QuickSearchIComponent {
     zoom: this.optionsSpec.zoom,
     center: L.latLng({ lat: this.optionsSpec.center[0], lng: this.optionsSpec.center[1] })
   };
+
+  fitBoundsOptions = {
+    padding: 100,
+    maxZoom: 10,
+    animate: true,
+    duration: 1
+  };
+
+  panOptions = {
+    animate: true,
+    duration: 1
+  };
+
+  zoomOptions = {
+    animate: true,
+    duration: 1
+  };
+
+  zoomPanOptions = {
+    animate: true,
+    duration: 1
+  };
+
+  onApply() {
+    this.zoom = this.model.zoom;
+    this.center = L.latLng([ this.model.latitude, this.model.longitude]);
+
+    return false;
+  }
 
 
   getSearch(){
@@ -111,4 +153,15 @@ export class QuickSearchIComponent {
 export interface TypeSearch{
   id: number;
   name: string;
+}
+
+export class LeafletCoreDemoModel {
+
+  constructor(
+      public latitude: number = 0,
+      public longitude: number = 0,
+      public zoom: number = 4,
+      public zoomLevels: number[] = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ]
+  ) { }
+
 }
