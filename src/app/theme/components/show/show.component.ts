58f5d0030c39fd10eef0f61978d876372c4a4321
nardm <lenar.thg@gmail.com>
2017-12-12 02:24:47 +0300

    new

--- src/app/theme/components/show/show.component.ts
+++ src/app/theme/components/show/show.component.ts
@@ -4,18 +4,14 @@ import {ChangeDetectionStrategy, ChangeDetectorRef, Component, NgZone, OnChanges
 import {SearchService} from "../../services/search/search.service";
 import * as L from 'leaflet';
 import 'leaflet.markercluster';
-declare var polylineM: any;
 import 'leaflet.polylinemeasure';
+import 'leaflet-search';
 import {MonumentInterface} from "../../model/quick-search";
 import Monuments = MonumentInterface.Monuments;
-import {DataSource} from "@angular/cdk/collections";
-import {Observable} from "rxjs/Observable";
-import {circle, latLng, polygon, tileLayer} from "leaflet";
 import {Subscription} from "rxjs/Subscription";
 import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
 import {AuthorInter} from "../#lib/table-data/table-data.component";
 import Spatial = AuthorInter.Spatial;
-import {LeafletLayersDemoModel} from "../quick-search/quick-search.component";
 import {PlatformLocation} from "@angular/common";
 
 
@@ -72,16 +68,43 @@ export class ShowIComponent implements OnChanges, OnInit {
 
   map;
 
-  ngOnInit(){
+  ngOnInit() {
     var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
           maxZoom: 18,
           attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Points &copy 2012 LINZ'
         }),
         latlng = L.latLng(55.798551, 49.106324);
-    this.map = L.map('map', {center: latlng, zoom: 3, layers: [tiles]});
-    L.control.scale({maxWidth:240, metric:true, imperial:false, position: 'bottomleft'}).addTo(this.map);
-    L.control.polylineMeasure({position:'topleft', unit:'metres', clearMeasurementsOnStop: false, showMeasurementsClearControl: true, showUnitControl: true}).addTo(this.map);
+    var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
 
+    var baseMaps = {
+      "Grayscale": tiles,
+      "Streets": OpenStreetMap_BlackAndWhite
+    };
+
+
+    this.map = L.map('map', {
+      center: latlng,
+      zoom: 3,
+      layers: [tiles,OpenStreetMap_BlackAndWhite]
+    });
+    L.control.layers(baseMaps).addTo(this.map);
+    L.control.scale({
+      maxWidth: 240,
+      metric: true,
+      imperial: false,
+      position: 'bottomleft'
+    }).addTo(this.map);
+
+    L.control.polylineMeasure({
+      position: 'topleft',
+      unit: 'metres',
+      clearMeasurementsOnStop: false,
+      showMeasurementsClearControl: true,
+      showUnitControl: true
+    }).addTo(this.map);
 
 
   }
@@ -143,9 +166,20 @@ export class ShowIComponent implements OnChanges, OnInit {
 
 
   onSpatial(spatial: Spatial[]){
-    let self = this;
-    var markers = L.markerClusterGroup();
+    let self =  this;
+      let markersLayer = new L.LayerGroup();	//layer contain searched elements
+    let markers = L.markerClusterGroup();
     let marker;
+
+    let controlSearch = new L.Control.Search({
+      position: 'topright',
+      layer: markersLayer,
+      initial: false,
+      zoom: 12,
+      marker: false
+    });
+
+    this.map.addControl(controlSearch);
     spatial.map(item => {
       let title: string = item.name;
       let marker_url: string = 'assets/icon/monTypes/monType' + item.type.id + '_' + item.epoch.id + '.png';
@@ -158,7 +192,7 @@ export class ShowIComponent implements OnChanges, OnInit {
             })});
       marker.bindPopup(title);
       markers.addLayer(marker);
-
+      markersLayer.addLayer(marker);
 
     });
     self.map.addLayer(markers);
